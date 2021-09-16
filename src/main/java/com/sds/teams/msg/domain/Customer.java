0f9ac9241855c5d32cb02804f093c944401c40ce
noveler17 <noveler17@gmail.com>
2021-09-16 17:00:28 +0900

    Bump up depedencies into the latest

--- src/main/java/com/sds/teams/msg/domain/Customer.java
+++ src/main/java/com/sds/teams/msg/domain/Customer.java
@@ -1,17 +1,15 @@
 package com.sds.teams.msg.domain;
 
-import lombok.Data;
-
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
 @Table(name = "customer")
-@Data
 public class Customer implements Serializable {
 
     private static final long serialVersionUID = -3009157732242241606L;
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
@@ -22,8 +20,7 @@ public class Customer implements Serializable {
     @Column(name = "lastname")
     private String lastName;
 
-    public Customer(){
-
+    protected Customer() {
     }
 
     public Customer(Long id, String firstName, String lastName) {
@@ -35,5 +32,51 @@ public class Customer implements Serializable {
     public Customer(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return id == customer.id && Objects.equals(firstName, customer.firstName) && Objects.equals(lastName, customer.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName);
     }
 }
